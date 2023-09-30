d8a584bc29faa2313c632993bad373fbaf817504
jieggii <jieggii@protonmail.com>
2023-09-30 21:41:48 +0300

    Add specific time to month bounds

--- src/utils/dateutils.js
+++ src/utils/dateutils.js
@@ -1,8 +1,8 @@
 export function CalculateMonthPeriod() {
     let currentDate = new Date();
 
-    let start = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-    let end = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    let start = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1, 0, 0, 0);
+    let end = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0, 23, 59, 59);
 
     return [start, end];
 }
